99b1371c8b0c862c7d1f2c72d1b856238fdfc469
Pavithra <pavi.aries4@gmail.com>
2023-04-24 19:10:49 +0530

    added component changes

--- demo-product/src/main/java/com/example/demoproduct/DemoProductApplication.java
+++ demo-product/src/main/java/com/example/demoproduct/DemoProductApplication.java
@@ -2,8 +2,10 @@ package com.example.demoproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
 public class DemoProductApplication {
 
 	public static void main(String[] args) {
